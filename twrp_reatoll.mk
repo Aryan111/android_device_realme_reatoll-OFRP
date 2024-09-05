14bfce0539d7fef418901d5a790d96d525346550
Aryan111 <45229110+Aryan111@users.noreply.github.com>
2024-09-05 10:07:09 +0530

    Update twrp_reatoll.mk

--- twrp_reatoll.mk
+++ twrp_reatoll.mk
@@ -19,6 +19,9 @@
 # Release name
 PRODUCT_RELEASE_NAME := reatoll
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 $(call inherit-product, device/realme/reatoll/device.mk)
 
 # Inherit some common TWRP stuff.
